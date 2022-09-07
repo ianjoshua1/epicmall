c7ff6107cd33b1bf638713b149d74f8a2dcd8b39
ianjoshua1 <ianjoshua002@gmail.com>
2022-09-07 20:16:55 +0800

    "initial commit"

---
+++ expand.js
@@ -0,0 +1,23 @@
+function expand() {
+  var x = document.getElementById("web");
+  
+  if(x.style.display === "none") {
+      x.style.display = "flex";
+  }
+
+  else {
+      x.style.display = "none";
+  }
+}
+
+function expand1() {
+  var x = document.getElementById("web1");
+  
+  if(x.style.display === "none") {
+      x.style.display = "flex";
+  }
+
+  else {
+      x.style.display = "none";
+  }
+}
